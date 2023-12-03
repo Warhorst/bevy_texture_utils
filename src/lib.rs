063c2fd6f0c23d0f23ab898a46c447f80dca543d
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-12-03 12:18:58 +0100

    The pixel size and tile dimensions are now parameterizable, using a creator struct storing them.

--- src/lib.rs
+++ src/lib.rs
@@ -2,144 +2,162 @@ use std::collections::HashMap;
 use bevy_asset::prelude::*;
 use bevy_render::prelude::*;
 use bevy_render::render_resource::{Extent3d, TextureDimension, TextureFormat};
+use bevy_render::texture::TextureFormatPixelInfo;
 use pad::{p, Position};
 
-const BYTES_PER_PIXEL: usize = 4;
-const TILE_DIMENSION: usize = 2;
-
-// TODO: tile dimension must be a parameter
 // TODO: Make sure every image uses the same TextureFormat
 
-/// Combine multiple given textures to a single one, forming
-/// a tile map texture.
-pub fn create_tile_map_texture(
-    images: &mut Assets<Image>,
-    positions_and_textures: impl IntoIterator<Item=(Position, Handle<Image>)>,
-) -> Result<Handle<Image>, &'static str> {
-    let position_texture_map = positions_and_textures
-        .into_iter()
-        .collect::<HashMap<_, _>>();
+/// Creates tile map textures.
+pub struct TileMapTextureCreator {
+    /// The expected texture format of every image
+    texture_format: TextureFormat,
+    /// The amount of bytes each pixel of the given textures consists of.
+    bytes_per_pixel: usize,
+    /// The expected width of each tile texture
+    tile_width: usize,
+    /// The expected height of each tile texture
+    tile_height: usize
+}
 
-    let max_x = get_max_x(&position_texture_map)?;
-    let min_x = get_min_x(&position_texture_map)?;
-    let max_y = get_max_y(&position_texture_map)?;
-    let min_y = get_min_y(&position_texture_map)?;
-
-    let width = (max_x - min_x) + 1;
-    let height = (max_y - min_y) + 1;
-
-    let mut data = vec![0u8; (width * TILE_DIMENSION * BYTES_PER_PIXEL) * (height * TILE_DIMENSION)];
-
-    for y in (min_y..=max_y).rev() {
-        for x in min_x..=max_x {
-            let absolute_pos = p!(x, y);
-            let relative_pos = p!(x - min_x, max_y - y);
-
-            let image = match position_texture_map.get(&absolute_pos) {
-                Some(handle) => match images.get(handle) {
-                    Some(image) => image,
-                    None => return Err("Not every image was already loaded")
-                },
-                None => continue,
-            };
-
-            let image_data = &image.data;
-
-            add_data_from_tile_image_at_position(width, &mut data, &relative_pos, image_data);
-        }
+impl TileMapTextureCreator {
+    pub fn new(texture_format: TextureFormat, tile_width: usize, tile_height: usize) -> Self {
+        Self { texture_format, bytes_per_pixel: texture_format.pixel_size(), tile_width, tile_height }
     }
 
-    let tiles_texture = create_image_from_data(width, height, data);
-    Ok(images.add(tiles_texture))
-}
+    /// Combine multiple given textures to a single one, forming
+    /// a tile map texture.
+    pub fn create_tile_map_texture(
+        &self,
+        images: &mut Assets<Image>,
+        positions_and_textures: impl IntoIterator<Item=(Position, Handle<Image>)>,
+    ) -> Result<Handle<Image>, &'static str> {
+        let position_texture_map = positions_and_textures
+            .into_iter()
+            .collect::<HashMap<_, _>>();
 
-fn get_max_x(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
-    let max_opt = position_texture_map
-        .keys()
-        .map(|pos| pos.x)
-        .max();
+        let max_x = Self::get_max_x(&position_texture_map)?;
+        let min_x = Self::get_min_x(&position_texture_map)?;
+        let max_y = Self::get_max_y(&position_texture_map)?;
+        let min_y = Self::get_min_y(&position_texture_map)?;
 
-    let max = match max_opt {
-        Some(max) => max,
-        None => return Err("No tiles were provided!")
-    };
+        let width = (max_x - min_x) + 1;
+        let height = (max_y - min_y) + 1;
 
-    Ok(max as usize)
-}
+        let mut data = vec![0u8; (width * self.tile_width * self.bytes_per_pixel) * (height * self.tile_height)];
 
-fn get_min_x(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
-    let min_opt = position_texture_map
-        .keys()
-        .map(|pos| pos.x)
-        .min();
+        for y in (min_y..=max_y).rev() {
+            for x in min_x..=max_x {
+                let absolute_pos = p!(x, y);
+                let relative_pos = p!(x - min_x, max_y - y);
 
-    let min = match min_opt {
-        Some(min) => min,
-        None => return Err("No tiles were provided!")
-    };
+                let image = match position_texture_map.get(&absolute_pos) {
+                    Some(handle) => match images.get(handle) {
+                        Some(image) => image,
+                        None => return Err("Not every image was already loaded")
+                    },
+                    None => continue,
+                };
 
-    Ok(min as usize)
-}
+                let image_data = &image.data;
 
-fn get_max_y(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
-    let max_opt = position_texture_map
-        .keys()
-        .map(|pos| pos.y)
-        .max();
+                self.add_data_from_tile_image_at_position(width, &mut data, &relative_pos, image_data);
+            }
+        }
 
-    let max = match max_opt {
-        Some(max) => max,
-        None => return Err("No tiles were provided!")
-    };
+        let tiles_texture = self.create_image_from_data(width, height, data);
+        Ok(images.add(tiles_texture))
+    }
 
-    Ok(max as usize)
-}
+    fn get_max_x(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
+        let max_opt = position_texture_map
+            .keys()
+            .map(|pos| pos.x)
+            .max();
 
-fn get_min_y(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
-    let min_opt = position_texture_map
-        .keys()
-        .map(|pos| pos.y)
-        .min();
+        let max = match max_opt {
+            Some(max) => max,
+            None => return Err("No tiles were provided!")
+        };
 
-    let min = match min_opt {
-        Some(min) => min,
-        None => return Err("No tiles were provided!")
-    };
+        Ok(max as usize)
+    }
 
-    Ok(min as usize)
-}
+    fn get_min_x(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
+        let min_opt = position_texture_map
+            .keys()
+            .map(|pos| pos.x)
+            .min();
 
-fn add_data_from_tile_image_at_position(width: usize, data: &mut [u8], pos: &Position, image_data: &[u8]) {
-    for y in 0..TILE_DIMENSION {
-        for x in 0..TILE_DIMENSION {
-            for i in 0..BYTES_PER_PIXEL {
-                let image_index = y * TILE_DIMENSION * BYTES_PER_PIXEL + x * BYTES_PER_PIXEL + i;
+        let min = match min_opt {
+            Some(min) => min,
+            None => return Err("No tiles were provided!")
+        };
 
-                let tiles_texture_index =
-                    (width * TILE_DIMENSION * BYTES_PER_PIXEL) * (pos.y as usize * TILE_DIMENSION) // move to the first row the tile is contained in
-                        + (pos.x as usize * TILE_DIMENSION * BYTES_PER_PIXEL) // than move to the first pixel of the tile
-                        + (TILE_DIMENSION * BYTES_PER_PIXEL * width * y) // than move to the current row of the tile
-                        + x * BYTES_PER_PIXEL // than move to the current pixel block
-                        + i; // than finally move to the current pixel
+        Ok(min as usize)
+    }
 
-                data[tiles_texture_index] = image_data[image_index];
+    fn get_max_y(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
+        let max_opt = position_texture_map
+            .keys()
+            .map(|pos| pos.y)
+            .max();
+
+        let max = match max_opt {
+            Some(max) => max,
+            None => return Err("No tiles were provided!")
+        };
+
+        Ok(max as usize)
+    }
+
+    fn get_min_y(position_texture_map: &HashMap<Position, Handle<Image>>) -> Result<usize, &'static str> {
+        let min_opt = position_texture_map
+            .keys()
+            .map(|pos| pos.y)
+            .min();
+
+        let min = match min_opt {
+            Some(min) => min,
+            None => return Err("No tiles were provided!")
+        };
+
+        Ok(min as usize)
+    }
+
+    fn add_data_from_tile_image_at_position(&self, width: usize, data: &mut [u8], pos: &Position, image_data: &[u8]) {
+        for y in 0..self.tile_height {
+            for x in 0..self.tile_width {
+                for i in 0..self.bytes_per_pixel {
+                    let image_index = y * self.tile_height * self.bytes_per_pixel + x * self.bytes_per_pixel + i;
+
+                    let tiles_texture_index =
+                        (width * self.tile_width * self.bytes_per_pixel) * (pos.y as usize * self.tile_height) // move to the first row the tile is contained in
+                            + (pos.x as usize * self.tile_width * self.bytes_per_pixel) // than move to the first pixel of the tile
+                            + (self.tile_height * self.bytes_per_pixel * width * y) // than move to the current row of the tile
+                            + x * self.bytes_per_pixel // than move to the current pixel block
+                            + i; // than finally move to the current pixel
+
+                    data[tiles_texture_index] = image_data[image_index];
+                }
             }
         }
     }
+
+    fn create_image_from_data(&self, max_x: usize, max_y: usize, data: Vec<u8>) -> Image {
+        Image::new(
+            Extent3d {
+                width: (max_x * self.tile_width) as u32,
+                height: (max_y * self.tile_height) as u32,
+                depth_or_array_layers: 1,
+            },
+            TextureDimension::D2,
+            data,
+            self.texture_format,
+        )
+    }
 }
 
-fn create_image_from_data(max_x: usize, max_y: usize, data: Vec<u8>) -> Image {
-    Image::new(
-        Extent3d {
-            width: (max_x * TILE_DIMENSION) as u32,
-            height: (max_y * TILE_DIMENSION) as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        data,
-        TextureFormat::Rgba8UnormSrgb,
-    )
-}
+
 
 #[cfg(test)]
 mod tests {
@@ -147,11 +165,12 @@ mod tests {
     use bevy_asset::prelude::*;
     use bevy_render::prelude::*;
     use bevy_render::render_resource::{Extent3d, TextureDimension, TextureFormat};
-    use crate::create_tile_map_texture;
+    use crate::{TileMapTextureCreator};
 
     #[test]
     fn create_tile_map_texture_works() {
         // arrange
+        let creator = TileMapTextureCreator::new(TextureFormat::Rgba8UnormSrgb, 2, 2);
         let mut images = Assets::<Image>::default();
         let red = images.add(create_image(
             (2, 2),
@@ -171,7 +190,7 @@ mod tests {
         ));
 
         // act
-        let image_result = create_tile_map_texture(
+        let image_result = creator.create_tile_map_texture(
             &mut images,
             [
                 (p!(0, 0), red.clone()),
