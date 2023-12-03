c1cf2ece0a26a0246fe96a6a3694edd085004538
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-12-03 16:34:45 +0100

    Added a convenience function to easily create a pixel mapper to replace pixels from one texture with another.

--- src/texture_modification.rs
+++ src/texture_modification.rs
@@ -1,16 +1,18 @@
 use bevy_render::prelude::*;
+use bevy_render::render_resource::Texture;
 
 pub type PixelBytes = [u8; 4];
 
-/// Modify the data of a texture with a given pixel mapper.
+/// Modify the data of a texture with a given pixel mapper. The mapper takes the x and y coordinates
+/// of the pixel and also the pixel bytes at these coordinates.
 /// TODO: Currently only works with 4-byte-pixel-images, will crash if something else is provided.
 pub fn modify_texture(
     texture: &mut Image,
-    pixel_mapper: impl Fn(PixelBytes) -> PixelBytes
+    pixel_mapper: impl Fn(usize, usize, PixelBytes) -> PixelBytes,
 ) {
+    let width = texture.width() as usize;
+    let height = texture.height() as usize;
     let data = &mut texture.data;
-    let width = texture.texture_descriptor.size.width as usize;
-    let height = texture.texture_descriptor.size.height as usize;
 
     for x in 0..width {
         for y in 0..height {
@@ -23,7 +25,7 @@ pub fn modify_texture(
                 data[index + 3],
             ];
 
-            let new_pixel = pixel_mapper(pixel);
+            let new_pixel = pixel_mapper(x, y, pixel);
 
             data[index] = new_pixel[0];
             data[index + 1] = new_pixel[1];
@@ -37,7 +39,7 @@ pub fn modify_texture(
 /// TODO: Currently only works with 4-byte-pixel-images, will crash if something else is provided.
 pub fn map_to_new_texture(
     texture: &Image,
-    pixel_mapper: impl Fn(PixelBytes) -> PixelBytes
+    pixel_mapper: impl Fn(usize, usize, PixelBytes) -> PixelBytes,
 ) -> Image {
     let mut new_image = texture.clone();
     modify_texture(&mut new_image, pixel_mapper);
@@ -45,12 +47,35 @@ pub fn map_to_new_texture(
     new_image
 }
 
+/// Provides a pixel mapper to replace the pixels of the original texture
+/// with ones from another texture. Also takes a pixel filter to tell
+/// if the pixel should be replaced by the pixel from the other texture.
+pub fn map_to_texture_pixels<'a>(
+    texture: &'a Image,
+    pixel_filter: fn(&PixelBytes) -> bool,
+) -> impl Fn(usize, usize, PixelBytes) -> PixelBytes + 'a {
+    move |x, y, pixel| if !pixel_filter(&pixel) {
+        pixel
+    } else {
+        let width = texture.width() as usize;
+        let x = x % width;
+        let y = y % texture.height() as usize;
+        let index = width * 4 * y + x * 4;
+        [
+            texture.data[index],
+            texture.data[index + 1],
+            texture.data[index + 2],
+            texture.data[index + 3]
+        ]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use bevy_render::prelude::*;
     use bevy_render::render_resource::TextureFormat;
     use crate::test_utils::create_image;
-    use crate::texture_modification::{map_to_new_texture, modify_texture};
+    use crate::texture_modification::{map_to_new_texture, map_to_texture_pixels, modify_texture};
 
     #[test]
     fn modify_texture_works() {
@@ -65,7 +90,7 @@ mod tests {
         );
 
         // act
-        modify_texture(&mut red_blue, |pixels| if pixels == Color::BLUE.as_rgba_u8() {
+        modify_texture(&mut red_blue, |_, _, pixels| if pixels == Color::BLUE.as_rgba_u8() {
             Color::GREEN.as_rgba_u8()
         } else {
             pixels
@@ -97,7 +122,7 @@ mod tests {
         );
 
         // act
-        let new_texture = map_to_new_texture(&red_blue, |pixels| if pixels == Color::BLUE.as_rgba_u8() {
+        let new_texture = map_to_new_texture(&red_blue, |_, _, pixels| if pixels == Color::BLUE.as_rgba_u8() {
             Color::GREEN.as_rgba_u8()
         } else {
             pixels
@@ -114,5 +139,49 @@ mod tests {
         );
 
         assert_eq!(expected.data, new_texture.data, "The red-blue image should now be mapped to a red-green one, but wasn't.");
+    }
+
+    /// Check if the convenience method to apply a texture on another one works. The
+    /// texture to map from is intentionally smaller to check if the mapper can
+    /// handle this.
+    #[test]
+    fn modify_texture_with_other_texture_works() {
+        // arrange
+        let mut red_blue = create_image(
+            (4, 4),
+            TextureFormat::Rgba8UnormSrgb,
+            [
+                Color::RED, Color::BLUE,Color::RED, Color::BLUE,
+                Color::BLUE, Color::RED, Color::BLUE, Color::RED,
+                Color::RED, Color::BLUE,Color::RED, Color::BLUE,
+                Color::BLUE, Color::RED, Color::BLUE, Color::RED,
+            ],
+        );
+
+        let yellow_green = create_image(
+            (2, 2),
+            TextureFormat::Rgba8UnormSrgb,
+            [
+                Color::YELLOW, Color::GREEN,
+                Color::YELLOW, Color::GREEN
+            ],
+        );
+
+        // act
+        modify_texture(&mut red_blue, map_to_texture_pixels(&yellow_green, |pixels| pixels == &Color::BLUE.as_rgba_u8()));
+
+        // assert
+        let expected = create_image(
+            (4, 4),
+            TextureFormat::Rgba8UnormSrgb,
+            [
+                Color::RED, Color::GREEN,Color::RED, Color::GREEN,
+                Color::YELLOW, Color::RED, Color::YELLOW, Color::RED,
+                Color::RED, Color::GREEN,Color::RED, Color::GREEN,
+                Color::YELLOW, Color::RED, Color::YELLOW, Color::RED,
+            ],
+        );
+
+        assert_eq!(expected.data, red_blue.data, "The red-blue texture should now be red-green-yellow, but wasn't.")
     }
 }
